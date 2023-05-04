435df0af9c6621565186223987cab0cfabd13874
Aleksandra Łapczuk <sandralapcz@gmail.com>
2023-05-05 00:02:38 +0200

    chat date and improvements

--- client/src/components/workspace/Chat.tsx
+++ client/src/components/workspace/Chat.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
 import {ChatBubbleOutline} from '@mui/icons-material';
 import ChatInput from './ChatInput';
@@ -7,33 +7,50 @@ import {useSocket} from '../../contexts/SocketContext';
 const Chat = () => {
 	const {socket, isConnected}: any = useSocket();
 
-	const [messages, setMessages] = useState<string[]>([]);
+	const [messages, setMessages] = useState<{text: string, timestamp: string}[]>([]);
+	const messageEndRef = useRef<HTMLDivElement>(null); // referencja do końca diva z wiadomościami
+
 	const handleSendMessage = (msg: { text: string, timestamp: string }) => {
 		if (!isConnected) return;
 
-		setMessages([...messages, msg.text]);
+		setMessages([...messages, { ...msg, timestamp: getCurrentTimestamp() }]);
 	};
-	
+	const getCurrentTimestamp = () => {
+		const date = new Date();
+		const year = date.getFullYear();
+		const month = ('0' + (date.getMonth() + 1)).slice(-2); // miesiące są liczone od 0 do 11, więc dodajemy 1 i formatujemy zerem z przodu
+		const day = ('0' + date.getDate()).slice(-2);
+		const hours = ('0' + date.getHours()).slice(-2);
+		const minutes = ('0' + date.getMinutes()).slice(-2);
+		const seconds = ('0' + date.getSeconds()).slice(-2);
+		return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+	};
+	useEffect(() => {
+		messageEndRef.current?.scrollIntoView({behavior: "auto"}); // przewijanie do końca diva z wiadomościami po zmianie zawartości
+	}, [messages]);
+
 	return (
 		<>
 			<List>
-				{['Chat'].map((text, index) => (
-					<ListItem key={text} disablePadding>
+				{['Chat'].map((message, index) => (
+					<ListItem  disablePadding>
 						<ListItemIcon>
 							<ChatBubbleOutline style={{marginLeft: 12}}/>
 						</ListItemIcon>
-						<ListItemText primary={text}/>
+						<ListItemText/>
 					</ListItem>
 				))}
 			</List>
 			<div style={{
+				display: "inline-block",
 				position: 'fixed',
-				bottom: 70,
 				overflowX: "hidden",
-				wordBreak: 'break-all',
-				width: "260px",
-				marginLeft: "4px",
-				marginRight: "2px"
+				wordBreak: 'normal',
+				width: "250px",
+				marginLeft: "2px",
+				marginRight: "2px",
+				maxHeight: "79%",
+				overflowY: "scroll"
 			}}>
 				{messages.map((message, index) => (
 					<div style={{
@@ -41,14 +58,16 @@ const Chat = () => {
 						borderRadius: "10px",
 						wordBreak: 'break-all',
 						marginTop: "3px",
-						marginBottom: "2px"
+						marginBottom: "5px"
 					}}
-					     key={index}>
-						{message}
+						 key={index}>
+						{message.text}
+						<div style={{fontSize: "smaller"}}>{message.timestamp}</div>
 					</div>
 				))}
+				<div ref={messageEndRef} /> {/* pusty div użyty do ustawienia referencji */}
 			</div>
-			<div style={{position: 'fixed', bottom: 0, margin: "2px", borderRadius: "10px"}}>
+			<div style={{position: 'fixed', bottom: "5px", margin: "2px", borderRadius: "10px"}}>
 				<ChatInput handleSendMessage={handleSendMessage}/>
 			</div>
 		</>
--- client/src/components/workspace/ChatInput.tsx
+++ client/src/components/workspace/ChatInput.tsx
@@ -1,29 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 import {Button} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import theme from "../../utils/theme";
 
 
-const ChatInput = ({handleSendMessage}: { handleSendMessage: (msg: {text: string, timestamp:string}) => void }) => {
+const ChatInput = ({handleSendMessage}: { handleSendMessage: (msg: { text: string, timestamp: string }) => void }) => {
     const [msg, setMsg] = useState("");
-    const sendChat = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const timestamp = Date.now().toLocaleString();
-        if (msg.length > 0) {
-            handleSendMessage({text: msg,timestamp});
-            setMsg("");
+        const sendChat = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            const timestamp = Date.now().toLocaleString();
+            if (msg.length > 0) {
+                handleSendMessage({text: msg, timestamp});
+                setMsg("");
+            }
         }
-    };
+    }
 
     return (
         <>
-            <form className="input-container" onSubmit={(event) => sendChat(event)}>
-                <input style={{backgroundColor: "lightgray"}}
-                       color="info"
-                       type="text"
-                       placeholder="Chat with your mates"
-                       onChange={(e) => setMsg(e.target.value)}
-                       value={msg}
+            <form className="input-container">
+                <textarea
+
+                    style={{marginLeft: "1px", backgroundColor: "lavenderblush", borderRadius: "10px", width: "100%"}}
+                    placeholder="Chat with your mates"
+                    onChange={(e) => setMsg(e.target.value)}
+                    value={msg}
+                    onKeyDown={(event) => sendChat(event)}
+                    rows={2}
+                    wrap="soft"
+                    aria-multiline={true}
                 />
                 <Button variant="contained" endIcon={<SendIcon/>} type="submit"
                         style={{backgroundColor: theme.palette.primary.main}}>
